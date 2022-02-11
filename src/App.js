6d5beaca21a6f7005e56d9cbc860a6231fd7e7dd
Kaviraj <ksb1910.07@gmail.com>
2022-02-11 11:40:00 +0530

    react-router-dom start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+const HatsPage = (props) => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
+const TopicList = () => {
+  return (
+    <div>
+      <h1>Topic list page</h1>
+    </div>
+  );
+};
+
+const TopicDetailPage = (props) => {
+  return (
+    <div>
+      <h1>Topic details page : {props.match.params.topicid}</h1>
+    </div>
+  );
+};
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/hats" component={HatsPage} />
+      <Route exact path="/topics" component={TopicList} />
+      <Route exact path="/topics/:topicid" component={TopicDetailPage} />
     </div>
   );
 }
