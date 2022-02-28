54b3a72a7c3ee04f67bc28e8b96851aa0894bce5
Kaviraj Singh Bhati <ksb1910.07@gmail.com>
2022-02-28 19:00:14 +0530

    router basic done

--- src/App.js
+++ src/App.js
@@ -11,28 +11,11 @@ const HatsPage = (props) => {
   );
 };
 
-const TopicList = () => {
-  return (
-    <div>
-      <h1>Topic list page</h1>
-    </div>
-  );
-};
-
-const TopicDetailPage = (props) => {
-  return (
-    <div>
-      <h1>Topic details page : {props.match.params.topicid}</h1>
-    </div>
-  );
-};
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
-      <Route exact path="/topics" component={TopicList} />
-      <Route exact path="/topics/:topicid" component={TopicDetailPage} />
+      <Route exact path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
